fb2a428bc15436de1bcf09a11634dcac75ccc735
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-20 18:46:44 +0000

    Add watch utils + expose snippets + make out dir configurable in wasm snippet

--- lib/default.nix
+++ lib/default.nix
@@ -3,13 +3,22 @@
   rustToolchain,
   ...
 }:
-with builtins; let
-  attrFromCargoToml = src: path: pkgs.lib.attrsets.getAttrFromPath path (fromTOML (readFile (src + "/Cargo.toml")));
+with builtins; 
+with pkgs.lib;
+let
+  attrFromCargoToml = src: path: attrsets.getAttrFromPath path (fromTOML (readFile (src + "/Cargo.toml")));
   callPackage = pkgs.lib.callPackageWith {inherit pkgs rustToolchain crane callPackage attrFromCargoToml;};
+  snippets = callPackage ./snippets.nix {};
 in {
   mkCrate = callPackage ./mkCrate.nix {};
   # TODO: mkWasmCrate does a bit more, it also generates JS bindings, its name should reflect this
   mkWasmCrate = callPackage ./mkWasmCrate.nix {};
 
   mkDevShell = callPackage ./mkDevShell.nix {};
+
+  inherit snippets;
+  
+  utils.watch = pathCmds:
+    foldl' (a: b: a + b)  ""
+      (attrsets.mapAttrsToList snippets.utils.watch pathCmds);
 }
--- lib/snippets.nix
+++ lib/snippets.nix
@@ -1,12 +1,24 @@
 {pkgs, ...}: {
   # TODO: make this configurable: ie: target, outdir, typscript, etc
-  wasm.bindgen = {}: ''
+  wasm.bindgen = {outDir ? "dist" }: ''
     for wasm in target/wasm32-unknown-unknown/release/*.wasm; do
       wasm-bindgen                                                          \
         --target web                                                        \
-        --out-dir dist                                                      \
+        --out-dir ${outDir}                                                 \
         --no-typescript                                                     \
         $wasm
     done
+  '';
+
+  utils.watch = path: cmd: let
+    cmdBinName = pkgs.lib.strings.sanitizeDerivationName ("watch-" + path + "cmd");
+    cmdBin = pkgs.writeShellScriptBin cmdBinName ''
+      echo "-> Changed ${path}..."
+      ${cmd}
+      echo "-> Done."
+    '';
+  in ''
+    ${cmdBin}/bin/${cmdBinName}
+    ${pkgs.fswatch}/bin/fswatch -o ${path} | xargs -n1 sh -c '${cmdBin}/bin/${cmdBinName}' &
   '';
 }
