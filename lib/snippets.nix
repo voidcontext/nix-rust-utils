b96986e9c2e9652bae77104281f723dba101d184
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-25 11:21:19 +0000

    Add snippets to help creating scripts that watch and serve web content

--- lib/snippets.nix
+++ lib/snippets.nix
@@ -10,6 +10,12 @@
     done
   '';
 
+  wasm.buildExample = name: ''
+    cargo build --release --example ${name} --target=wasm32-unknown-unknown
+
+    cp target/wasm32-unknown-unknown/release/examples/${name}.wasm target/wasm32-unknown-unknown/release
+  '';
+
   utils.watch = path: cmd: let
     cmdBinName = pkgs.lib.strings.sanitizeDerivationName ("watch-" + path + "cmd");
     cmdBin = pkgs.writeShellScriptBin cmdBinName ''
@@ -20,5 +26,29 @@
   in ''
     ${cmdBin}/bin/${cmdBinName}
     ${pkgs.fswatch}/bin/fswatch -o ${path} | xargs -n1 sh -c '${cmdBin}/bin/${cmdBinName}' &
+  '';
+
+  utils.serve = src: port: ''
+    ${pkgs.simple-http-server}/bin/simple-http-server \
+      -p ${builtins.toString port}                    \
+      --nocache                                       \
+      -i --try-file ${src}/index.html                 \
+      -- ${src}
+  '';
+
+  utils.cleanupWrapper = body: ''
+    # https://unix.stackexchange.com/a/55922
+    trap 'cleanup' INT TERM
+    cleanup() {
+        trap "" INT TERM  # ignore INT and TERM while shutting down
+        echo "**** Shutting down... ****"
+        ${pkgs.coreutils}/bin/kill -TERM 0
+        wait
+        echo DONE
+    }
+
+    ${body}
+
+    cleanup
   '';
 }
