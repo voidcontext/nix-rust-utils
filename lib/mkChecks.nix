1b5fdf03dd5e96ce3528c3607286947559ae1e27
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-10-18 19:05:30 +0000

    fix: handle cargo clippy extra args (#9)
    Reviewed-on: https://git.vdx.hu/voidcontext/nix-rust-utils/pulls/9
    Co-authored-by: Gabor Pihaj <gabor.pihaj@gmail.com>
    Co-committed-by: Gabor Pihaj <gabor.pihaj@gmail.com>

--- lib/mkChecks.nix
+++ lib/mkChecks.nix
@@ -9,9 +9,11 @@
   buildInputs ? [],
   nativeBuildInputs ? [],
   cargoExtraArgs ? "",
+  cargoClippyExtraArgs ? "--all-targets -- -Dwarnings -W clippy::pedantic",
   target ? null,
   nextest ? false,
-}: let
+  ...
+} @ args: let
   cargoArtifacts = import ./internal/mkArtifacts.nix {
     inherit
       pkgs
@@ -26,6 +28,7 @@
   utils = import ./utils.nix {inherit pkgs craneLib;};
   commonArgs = utils.commonArgs {inherit src nativeBuildInputs buildInputs target cargoExtraArgs;};
   commonArgsWithoutTarget = utils.commonArgs {inherit src nativeBuildInputs buildInputs cargoExtraArgs;};
+  cleanedArgs = builtins.removeAttrs args ["crate" "src" "target" "nextest" "cargoClippyExtraArgs"];
 in
   {
     # Build the crate as part of `nix flake check` for convenience
@@ -37,27 +40,30 @@ in
     # Note that this is done as a separate derivation so that
     # we can block the CI if there are issues here, but not
     # prevent downstream consumers from building our crate by itself.
-    cargo-clippy = craneLib.cargoClippy (commonArgsWithoutTarget
+    cargo-clippy = craneLib.cargoClippy (cleanedArgs
+      // commonArgsWithoutTarget
       // {
-        inherit cargoArtifacts;
-        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        inherit cargoArtifacts cargoClippyExtraArgs;
       });
 
-    cargo-doc = craneLib.cargoDoc (commonArgsWithoutTarget
+    cargo-doc = craneLib.cargoDoc (cleanedArgs
+      // commonArgsWithoutTarget
       // {
         inherit cargoArtifacts;
       });
 
     # Check formatting
-    cargo-fmt = craneLib.cargoFmt {
-      inherit (commonArgs) src;
-    };
+    cargo-fmt = craneLib.cargoFmt (cleanedArgs
+      // {
+        inherit (commonArgs) src;
+      });
 
     # Audit dependencies
-    cargo-audit = craneLib.cargoAudit {
-      inherit (commonArgs) src;
-      inherit advisory-db;
-    };
+    cargo-audit = craneLib.cargoAudit (cleanedArgs
+      // {
+        inherit (commonArgs) src;
+        inherit advisory-db;
+      });
   }
   // (
     if nextest
@@ -65,7 +71,8 @@ in
       # Run tests with cargo-nextest
       # Consider setting `doCheck = false` on `my-crate` if you do not want
       # the tests to run twice
-      cargo-nextest = craneLib.cargoNextest (commonArgs
+      cargo-nextest = craneLib.cargoNextest (cleanedArgs
+        // commonArgs
         // {
           inherit cargoArtifacts;
           partitions = 1;
