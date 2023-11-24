34c8e0765dd358416907aaaaf0e1abd724b16ee1
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-11-24 17:51:12 +0000

    fix: allow passing environment variables to the dev shell (#12)
    Reviewed-on: https://git.vdx.hu/voidcontext/nix-rust-utils/pulls/12
    Co-authored-by: Gabor Pihaj <gabor.pihaj@gmail.com>
    Co-committed-by: Gabor Pihaj <gabor.pihaj@gmail.com>

--- lib/mkDevShell.nix
+++ lib/mkDevShell.nix
@@ -6,20 +6,24 @@
   inputsFrom ? [],
   checks ? {},
   packages ? [],
-}:
-craneLib.devShell {
-  inputsFrom = (builtins.attrValues checks) ++ inputsFrom;
+  ...
+} @ args: let
+  cleanedArgs = builtins.removeAttrs args ["inputsFrom" "checks" "packages"];
+in
+  craneLib.devShell ({
+      inputsFrom = (builtins.attrValues checks) ++ inputsFrom;
 
-  packages =
-    [
-      pkgs.cargo-outdated
-      pkgs.cargo-watch
-      pkgs.cargo-bloat
-      pkgs.cargo-udeps
-      pkgs.cargo-edit
-      pkgs.rust-analyzer
-      pkgs.rustfmt
-      pkgs.nixpkgs-fmt
-    ]
-    ++ packages;
-}
+      packages =
+        [
+          pkgs.cargo-outdated
+          pkgs.cargo-watch
+          pkgs.cargo-bloat
+          pkgs.cargo-udeps
+          pkgs.cargo-edit
+          pkgs.rust-analyzer
+          pkgs.rustfmt
+          pkgs.nixpkgs-fmt
+        ]
+        ++ packages;
+    }
+    // cleanedArgs)
